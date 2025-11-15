7d115c695725b1e869cbaf26c72f7ac36d374669
Mantsha Khan <168266294+Mantshakhan10@users.noreply.github.com>
2025-11-15 20:24:14 +0530

    Added Week-1-Day-5-Bonus-Task

---
+++ Week-1/Day-5/Bonus-Task/script.js
@@ -0,0 +1,11 @@
+function lightMode() {
+    let page = document.getElementById("page");
+    page.style.background = "white";
+    page.style.color = "black";
+}
+
+function darkMode() {
+    let page = document.getElementById("page");
+    page.style.background = "black";
+    page.style.color = "white";
+}
