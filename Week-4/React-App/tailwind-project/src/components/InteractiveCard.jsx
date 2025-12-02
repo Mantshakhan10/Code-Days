f9fbdd44dc0b7d3cf4c95da31082ac538f71d181
Mantshakhan10 <mantshakhan5020@gmail.com>
2025-12-02 23:45:07 +0530

    Added Week-4-Day-4

--- Week-4/React-App/tailwind-project/src/components/InteractiveCard.jsx
+++ Week-4/React-App/tailwind-project/src/components/InteractiveCard.jsx
@@ -27,9 +27,7 @@ function InteractiveCard() {
       <button
         className="
         mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg
-        hover:bg-blue-600
-        active:scale-95
-        transition-all duration-300
+      hover:bg-blue-600 transition
       "
       >
         Read More
