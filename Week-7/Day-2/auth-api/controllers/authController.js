a74751d28a238338b3f5f63a50c6d8ad32261c81
Mantshakhan10 <mantshakhan5020@gmail.com>
2025-12-24 16:06:00 +0530

    Added Week-7-Day-2

---
+++ Week-7/Day-2/auth-api/controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // 1️⃣ Validate input
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // 2️⃣ Check duplicate email
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    // 3️⃣ Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 4️⃣ Save user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    // 5️⃣ Response (never send password)
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
